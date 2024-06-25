d9342cf734034d5bbbc2003528653b9dd666d4b0
hyunutae <lx140616@gmail.com>
2024-06-25 15:29:20 +0900

    6/25 중간 업데이트
    ----완료-------
    1. 폴리곤 그려서 지적도 객체 선택하기 드래그박스X
    2. 읍면동 선택 메뉴, 지표 입력 메뉴 설정
    3. 초기 화면에 읍면동 구역 지도만 뜨게
    4. 읍면동 선택시 해당 구역 지적도 뜨게
    5. 읍면동 선택으로 돌아가기 버튼으로 다시 선택 가능하게
    6. 읍면동 선택시 읍면동 메뉴 사라지면서 평가지표 입력창만 남게
    7. 범위 선택시 콤보박스에 선택된 지번 리스트 나오게
    8. 지번 선택시 선택된 지번 화면에 보여주고 해당되는 db값 호출
    9. 조회버튼 눌러서 db값 나오도록(경사도만 구현)
    10. 지표값 입력하여 jsp로 넘어가도록 하기
    11. jsp로 넘어간 지표값 db에 입력및수정
    12. 업데이트 된 db값 바로 조회되게 처리 + 다른 지번 조회 후 다시 돌아가도 조회되도록 처리
    13. 지표값 테이블 형태 수정
    14. 전체지역 선택 추가 - 느려서 사용 어려움
    15. 팝업창 추가 - 일단 지번만 나옴
    16. 팝업창에 기본 정보 추가
    17. 메뉴2만 스크롤되게 수정
    18. 지표값 span으로 표현해서 수정 안되게
    19. 점수, 적성값, 종합값 등 테이블 및 버튼 수정(아직 비어있음)
    --------해야할것----------
    db 정리 및 변수값 통일
    모든 값 입력 및 조회 후 등급 분류
    분류된 등급에 맞춰 지도에 색으로 표현

--- prj1/main.js
+++ prj1/main.js
@@ -6,7 +6,7 @@ import GeoJSON from 'ol/format/GeoJSON.js';
 import { Map, Overlay, View } from 'ol';
 import Tile from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import { Select, Draw,} from 'ol/interaction.js';
+import { Select, Draw, } from 'ol/interaction.js';
 import Feature from 'ol/Feature.js';
 
 var vectorLayer;
@@ -32,16 +32,16 @@ const centers = {
 };
 
 const centers2 = {
-  'emd00': [14086748,4154096],
+  'emd00': [14086748, 4154096],
   'emd01': [14071824, 4165714],
-  'emd02': [14086748,4154096],
-  'emd03': [14084063,4165091],
-  'emd04': [14081935,4142747],
-  'emd05': [14068863,4158200],
-  'emd06': [14088420,4142392],
-  'emd07': [14078743,4155363],
-  'emd08': [14065164,4181305],
-  'emd09': [14079972,4170940],
+  'emd02': [14086748, 4154096],
+  'emd03': [14084063, 4165091],
+  'emd04': [14081935, 4142747],
+  'emd05': [14068863, 4158200],
+  'emd06': [14088420, 4142392],
+  'emd07': [14078743, 4155363],
+  'emd08': [14065164, 4181305],
+  'emd09': [14079972, 4170940],
 };
 
 // 초기 지도 중심과 확대 수준
@@ -105,7 +105,7 @@ function makeWFSSource2(clickedFeatureJibun) {
     url: encodeURI('http://localhost:42888/geoserver/campWS/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=campWS:muan_all2&outputFormat=application/json&CQL_FILTER=' + makeFilter(clickedFeatureJibun)),
     format: new GeoJSON(),
   });
-  if (null != selectLayer){
+  if (null != selectLayer) {
     selectLayer.setSource(vectorSource3);
     selectLayer.setVisible(true);
   }
@@ -215,8 +215,8 @@ const displayFeatureInfo = function (pixel, target) {
   const feature = target.closest('.ol-control')
     ? undefined
     : map.forEachFeatureAtPixel(pixel, function (feature) {
-        return feature;
-      });
+      return feature;
+    });
   if (feature) {
     console.log("feature:" + feature);
     info.style.left = pixel[0] + 'px';
@@ -315,112 +315,112 @@ function polyIntersectsPoly(polygeomA, polygeomB) {
 draw.on('drawstart', function () {
   selectedFeatures.clear();
 });
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const aaa01 = document.getElementById('aaa01');
-const comboBox = document.getElementById('selectedRegions');
-const selectedValueDisplay = document.getElementById('selectedValueDisplay');
-const selectedValueDisplay2 = document.getElementById('selectedValueDisplay2');
+  const comboBox = document.getElementById('selectedRegions');
+  const selectedValueDisplay = document.getElementById('selectedValueDisplay');
+  const selectedValueDisplay2 = document.getElementById('selectedValueDisplay2');
 
-// 콤보박스 값 변경 시 선택된 값을 표시 및 변수에 저장
-let clickedFeatureJibun = null;
-let clickedFeatureA1 = null; // pnu
-let clickedFeatureA20 = null; // 경사도(ex: 완만함)
-let clickedFeatureA11 = null; // 지목
-let clickedFeatureA12 = null; // 면적
-let clickedFeatureA14 = null; // 용도지역
-let clickedFeatureA22 = null; // 토지형태
-let clickedFeatureA24 = null; // 도로인접여부
-let insertedA20 = {};
+  // 콤보박스 값 변경 시 선택된 값을 표시 및 변수에 저장
+  let clickedFeatureJibun = null;
+  let clickedFeatureA1 = null; // pnu
+  let clickedFeatureA20 = null; // 경사도(ex: 완만함)
+  let clickedFeatureA11 = null; // 지목
+  let clickedFeatureA12 = null; // 면적
+  let clickedFeatureA14 = null; // 용도지역
+  let clickedFeatureA22 = null; // 토지형태
+  let clickedFeatureA24 = null; // 도로인접여부
+  let insertedA20 = {};
 
-selectedFeatures.on(['add', 'remove'], function () {
-  const names = selectedFeatures.getArray().map(function (feature) {
-    const jibun = feature.get('jibun') || '필지 선택';
-    const a1 = feature.get('a1') || '';  // a1 값을 가져옴
-    const a11 = feature.get('a11') || '';  // a11 값을 가져옴
-    const a12 = feature.get('a12') || '';  // a12 값을 가져옴
-    const a14 = feature.get('a14') || '';  // a14 값을 가져옴
-    const a20 = feature.get('a20') || '';  // a20 값을 가져옴
-    const a22 = feature.get('a22') || '';  // a22 값을 가져옴
-    const a24 = feature.get('a24') || '';  // a24 값을 가져옴
-    return { jibun, a1, a11, a12, a14, a20, a22, a24 };
+  selectedFeatures.on(['add', 'remove'], function () {
+    const names = selectedFeatures.getArray().map(function (feature) {
+      const jibun = feature.get('jibun') || '필지 선택';
+      const a1 = feature.get('a1') || '';  // a1 값을 가져옴
+      const a11 = feature.get('a11') || '';  // a11 값을 가져옴
+      const a12 = feature.get('a12') || '';  // a12 값을 가져옴
+      const a14 = feature.get('a14') || '';  // a14 값을 가져옴
+      const a20 = feature.get('a20') || '';  // a20 값을 가져옴
+      const a22 = feature.get('a22') || '';  // a22 값을 가져옴
+      const a24 = feature.get('a24') || '';  // a24 값을 가져옴
+      return { jibun, a1, a11, a12, a14, a20, a22, a24 };
+    });
+    updateComboBox(names);
   });
-  updateComboBox(names);
-});
 
-function updateComboBox(features) {
-  comboBox.innerHTML = '';
-  features.forEach(function (feature) {
-    if (feature.jibun) {
-      const option = document.createElement('option');
-      option.value = feature.jibun;
-      option.text = feature.jibun;
-      option.setAttribute('a1', feature.a1);  // a1 값을 데이터 속성으로 추가
-      option.setAttribute('a11', feature.a11);  // a11 값을 데이터 속성으로 추가
-      option.setAttribute('a12', feature.a12);  // a12 값을 데이터 속성으로 추가
-      option.setAttribute('a14', feature.a14);  // a14 값을 데이터 속성으로 추가
-      option.setAttribute('a20', feature.a20);  // a20 값을 데이터 속성으로 추가
-      option.setAttribute('a22', feature.a22);  // a22 값을 데이터 속성으로 추가
-      option.setAttribute('a24', feature.a24);  // a24 값을 데이터 속성으로 추가
-      comboBox.appendChild(option);
-    }
+  function updateComboBox(features) {
+    comboBox.innerHTML = '';
+    features.forEach(function (feature) {
+      if (feature.jibun) {
+        const option = document.createElement('option');
+        option.value = feature.jibun;
+        option.text = feature.jibun;
+        option.setAttribute('a1', feature.a1);  // a1 값을 데이터 속성으로 추가
+        option.setAttribute('a11', feature.a11);  // a11 값을 데이터 속성으로 추가
+        option.setAttribute('a12', feature.a12);  // a12 값을 데이터 속성으로 추가
+        option.setAttribute('a14', feature.a14);  // a14 값을 데이터 속성으로 추가
+        option.setAttribute('a20', feature.a20);  // a20 값을 데이터 속성으로 추가
+        option.setAttribute('a22', feature.a22);  // a22 값을 데이터 속성으로 추가
+        option.setAttribute('a24', feature.a24);  // a24 값을 데이터 속성으로 추가
+        comboBox.appendChild(option);
+      }
+    });
+  } 
+
+  // 콤보박스 값 변경 시 선택된 지번을 표시
+  comboBox.addEventListener('change', function () {
+    selectedValueDisplay.textContent = comboBox.value;
+    selectedValueDisplay2.textContent = comboBox.value.substring(4);
+    clickedFeatureJibun = comboBox.value;
+    const selectedOption = comboBox.options[comboBox.selectedIndex];
+    clickedFeatureA1 = selectedOption.getAttribute('a1'); // pnu
+    clickedFeatureA20 = selectedOption.getAttribute('a20'); // 경사도
+    clickedFeatureA11 = selectedOption.getAttribute('a11'); // 지목
+    clickedFeatureA12 = selectedOption.getAttribute('a12'); // 면적
+    clickedFeatureA14 = selectedOption.getAttribute('a14'); // 용도지역
+    clickedFeatureA22 = selectedOption.getAttribute('a22'); // 토지형태
+    clickedFeatureA24 = selectedOption.getAttribute('a24'); // 도로인접여부
+    console.log("clickedFeatureA14:" + clickedFeatureA14);
+    document.getElementById("fetchData_link").href = "./fetchData.jsp?selectedA1='" + clickedFeatureA1 + "'";
+
+    // 필드 값 비우기
+    document.getElementById("aaa01").value = '';
+
+    // selectLayer 업데이트 및 표시
+    makeWFSSource2(clickedFeatureJibun);
+
+    return clickedFeatureA1, clickedFeatureA20, clickedFeatureA11, clickedFeatureA12, clickedFeatureA14, clickedFeatureA22, clickedFeatureA24, clickedFeatureJibun;
   });
-}
-
-// 콤보박스 값 변경 시 선택된 지번을 표시
-comboBox.addEventListener('change', function () {
-  selectedValueDisplay.textContent = comboBox.value;
-  selectedValueDisplay2.textContent = comboBox.value.substring(4);
-  clickedFeatureJibun = comboBox.value;
-  const selectedOption = comboBox.options[comboBox.selectedIndex];
-  clickedFeatureA1 = selectedOption.getAttribute('a1'); // pnu
-  clickedFeatureA20 = selectedOption.getAttribute('a20'); // 경사도
-  clickedFeatureA11 = selectedOption.getAttribute('a11'); // 지목
-  clickedFeatureA12 = selectedOption.getAttribute('a12'); // 면적
-  clickedFeatureA14 = selectedOption.getAttribute('a14'); // 용도지역
-  clickedFeatureA22 = selectedOption.getAttribute('a22'); // 토지형태
-  clickedFeatureA24 = selectedOption.getAttribute('a24'); // 도로인접여부
-  console.log("clickedFeatureA14:" + clickedFeatureA14);
-  document.getElementById("fetchData_link").href = "./fetchData.jsp?selectedA1='" + clickedFeatureA1 + "'";
-
-  // 필드 값 비우기
-  document.getElementById("aaa01").value = '';
-
-  // selectLayer 업데이트 및 표시
-  makeWFSSource2(clickedFeatureJibun);
-
-  return clickedFeatureA1,clickedFeatureA20,clickedFeatureA11,clickedFeatureA12,clickedFeatureA14,clickedFeatureA22,clickedFeatureA24, clickedFeatureJibun;
-});
 
 
-// 조회 버튼 클릭시 db에 저장된 지표값 표시
-document.getElementById('searchData').onclick = () => {
-  if (insertedA20[clickedFeatureA1] === undefined) {
-    console.log('insertedA20 is null for current A1');
-    // document.getElementById("aaa01").value = clickedFeatureA20;
-    aaa01.innerText = clickedFeatureA20; // aaa01에 값 설정
-  } else {
-    console.log('insertedA20 is not null for current A1');
-    // document.getElementById("aaa01").value = insertedA20[clickedFeatureA1];
-    aaa01.innerText = insertedA20[clickedFeatureA1]; // aaa01에 값 설정
-  }
-  // 팝업 표시
-  const selectedFeature = vectorSource.getFeatures().find(feature => feature.get('jibun') === clickedFeatureJibun);
-  if (selectedFeature) {
-    document.getElementById("info-title").innerHTML = clickedFeatureJibun;
-    document.getElementById("info01").innerHTML = "지목: "+clickedFeatureA11;
-    document.getElementById("info02").innerHTML = "면적: "+clickedFeatureA12 +"㎡";
-    document.getElementById("info03").innerHTML = "용도지역: "+clickedFeatureA14;
-    document.getElementById("info04").innerHTML = "토지형태: "+clickedFeatureA22;
-    document.getElementById("info05").innerHTML = "도로인접면: "+clickedFeatureA24;
-
-    if (centers2[emdid]) {
-      const selectedCenter = centers2[emdid];
-      overlay.setPosition(selectedCenter);
+  // 조회 버튼 클릭시 db에 저장된 지표값 표시
+  document.getElementById('searchData').onclick = () => {
+    if (insertedA20[clickedFeatureA1] === undefined) {
+      console.log('insertedA20 is null for current A1');
+      // document.getElementById("aaa01").value = clickedFeatureA20;
+      aaa01.innerText = clickedFeatureA20; // aaa01에 값 설정
     } else {
-      console.log('Selected center not found in centers object');
+      console.log('insertedA20 is not null for current A1');
+      // document.getElementById("aaa01").value = insertedA20[clickedFeatureA1];
+      aaa01.innerText = insertedA20[clickedFeatureA1]; // aaa01에 값 설정
     }
-  }
-};
+    // 팝업 표시
+    const selectedFeature = vectorSource.getFeatures().find(feature => feature.get('jibun') === clickedFeatureJibun);
+    if (selectedFeature) {
+      document.getElementById("info-title").innerHTML = clickedFeatureJibun;
+      document.getElementById("info01").innerHTML = "지목: " + clickedFeatureA11;
+      document.getElementById("info02").innerHTML = "면적: " + clickedFeatureA12 + "㎡";
+      document.getElementById("info03").innerHTML = "용도지역: " + clickedFeatureA14;
+      document.getElementById("info04").innerHTML = "토지형태: " + clickedFeatureA22;
+      document.getElementById("info05").innerHTML = "도로인접면: " + clickedFeatureA24;
+
+      if (centers2[emdid]) {
+        const selectedCenter = centers2[emdid];
+        overlay.setPosition(selectedCenter);
+      } else {
+        console.log('Selected center not found in centers object');
+      }
+    }
+  };
 
 });
 
@@ -434,7 +434,7 @@ document.getElementById('insertData').onclick = () => {
   localStorage.setItem('insertedA20', JSON.stringify(insertedA20));
 
   sendData(clickedFeatureA1, value);
-  
+
 }
 
 // 서버로 데이터 전송
